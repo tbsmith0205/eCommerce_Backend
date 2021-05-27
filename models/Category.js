411d777f7e7c2c45eb19f00aca3618c3761d5163
Taylor Smith <tbsmith0205@gmail.com>
2021-05-27 15:55:12 -0600

    defined columns in Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,25 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -1,20 +1,21 @@
-const { Category } = require('../models');
+const sequelize = require("../config/connection");
+const { Category } = require("../models");
 
 const categoryData = [
   {
-    category_name: 'Shirts',
+    category_name: "Shirts",
   },
   {
-    category_name: 'Shorts',
+    category_name: "Shorts",
   },
   {
-    category_name: 'Music',
+    category_name: "Music",
   },
   {
-    category_name: 'Hats',
+    category_name: "Hats",
   },
   {
-    category_name: 'Shoes',
+    category_name: "Shoes",
   },
 ];
 
