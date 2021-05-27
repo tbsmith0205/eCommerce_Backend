39574a4a2a6fce80a41dcff0648b8ab608f2232d
Taylor Smith <tbsmith0205@gmail.com>
2021-05-27 17:04:59 -0600

    fixed errors committed when setting up models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -16,6 +16,13 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
         model: "tag",
         key: "id",
       },
