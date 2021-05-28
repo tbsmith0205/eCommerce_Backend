5315e967c2eeed4443f0988a0596027d67ff6955
Taylor Smith <tbsmith0205@gmail.com>
2021-05-28 15:58:22 -0600

    completed all routes in product-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    categoryData = await Category.findAll({
+    const categoryData = await Category.findAll({
       include: {
         model: Product,
       },
